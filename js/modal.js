df248d8993f7d285cb45672835aaea46399d5eb4
Zhabrov-Ilya <ilya.Zhabrow@yandex.ru>
2022-03-30 17:23:45 +0300

    Добавлено модальное окно, адаптирован аккордеон для разрешения от 1024px, добавлены hover-эффекты, + мелкие исправления

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const blurContainer = document.querySelector('#blur-container');
+const closeBtn = document.querySelector('.modal__cross');
+
+closeBtn.addEventListener('click', function () {
+  modal.classList.remove('animate__bounceIn');
+  modal.classList.add('animate__bounceOut');
+  blurContainer.classList.add('no-blur');
+  modal.addEventListener('animationend', () => {
+    modal.classList.add('hidden');
+    blurContainer.classList.remove('blur', 'disabled');
+  });
+});
+
+window.addEventListener('load', () => {
+  const mediaQuery = window.matchMedia('(min-width: 768px)');
+  if (!mediaQuery.matches) {
+    return;
+  }
+  setTimeout(function() {
+    modal.classList.remove('hidden');
+    modal.classList.add('animate__animated', 'animate__bounceIn');
+    blurContainer.classList.remove('no-blur');
+    blurContainer.classList.add('blur', 'disabled');
+  }, 2000);
+});
